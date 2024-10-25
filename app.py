5757a8eb9c48fc3ad4d0eba82ac7f88befe8b009
Ahmed Kettaf <kettafahmed999@gmail.com>
2024-10-25 17:04:34 +0100

    Update app for redeployment

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, jsonify
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -18,4 +19,6 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # الحصول على المنفذ من المتغيرات البيئية
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
